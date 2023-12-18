b4f32ecc6a38ffdd466f6bd940f3df0d4b36ce37
HooJungKim <80149444+HooJungKim@users.noreply.github.com>
2023-12-18 17:53:10 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	queue <int> q;
+
+	while (n--) {
+		string str;
+		cin >> str;
+
+		if (str == "push") {
+			int num;
+			cin >> num;
+			q.push(num);
+		}
+		else if (str == "pop") {
+			if (q.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (str == "size") {
+			cout << q.size() << '\n';
+		}
+		else if (str == "empty") {
+			cout << (int)q.empty() << '\n';
+		}
+		else if (str == "front") {
+			if (q.empty()) {
+				cout << -1 << '\n';
+			}
+			else{
+				cout << q.front() << '\n';
+			}			
+		}
+		else if (str == "back") {
+			if (q.empty()) {
+				cout << -1 << '\n';
+			}
+			else{
+				cout << q.back() << '\n';
+			}
+		}
+	}
+}
