ba07f16cf035b4c55160067fa06b6cc6a7714fda
HooJungKim <80149444+HooJungKim@users.noreply.github.com>
2023-12-26 17:05:12 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int Fib(int n) {
+	int arr[46];
+	arr[0] = 0;
+	arr[1] = 1;
+	for (int i = 2; i < 46; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	return arr[n];
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	long long n;
+	cin >> n;
+	cout << Fib(n);
+}
