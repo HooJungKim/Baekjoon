d1fad12405ec185e5ab714f26b8eda321b3c3433
HooJungKim <80149444+HooJungKim@users.noreply.github.com>
2024-02-16 22:55:59 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	vector<int> v(n+1);
+	v[1] = 1;
+	v[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		v[i] = (v[i - 1] + v[i - 2]) % 10007;
+	}
+	cout << v[n];
+}
