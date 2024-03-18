7335315e76a308907755f1cd98be23bb552a67fe
HooJungKim <80149444+HooJungKim@users.noreply.github.com>
2024-03-18 21:28:34 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+#define M 1234567891
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -10,11 +11,12 @@ int main() {
 	string s;
 	cin >> n >> s;
 	long long sum = 0;
-	long long x;
+	long long x, r = 1;
 
 	for (int i = 0; i < s.size(); i++) {
-		x = (s[i]-'a' + 1) * pow(31, i);
-		sum += x % 1234567891;
+		sum += (s[i] - 'a' + 1) * r % M;
+		sum %= M;
+		r = (r * 31) % M;
 	}
 	cout << sum;
 }
