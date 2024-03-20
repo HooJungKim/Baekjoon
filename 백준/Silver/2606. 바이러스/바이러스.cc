b5e470bc58f886f65ed25c34551bbff2a6e4a2aa
HooJungKim <80149444+HooJungKim@users.noreply.github.com>
2024-03-20 16:38:35 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+vector<int> adj[1001];
+bool visit[1001];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		int st, end;
+		cin >> st >> end;
+		adj[st].push_back(end);
+		adj[end].push_back(st);
+	}
+
+	queue<int> q;
+	q.push(1);
+	visit[1] = true;
+	int answer = 0;
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		for (int nxt : adj[cur]) {
+			if (visit[nxt]) continue;
+			q.push(nxt);
+			visit[nxt] = true;
+			answer++;
+		}
+	}
+	cout << answer;
+}
